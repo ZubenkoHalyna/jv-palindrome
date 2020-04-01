82de0ea03adb579ba6428ecd89e2c4b543a3cfa7
Galina <gala6ap@mail.ru>
2020-04-01 14:19:41 +0300

    isPalindrome implementation added

--- src/main/java/core/basesyntax/Palindrome.java
+++ src/main/java/core/basesyntax/Palindrome.java
@@ -21,6 +21,8 @@ public class Palindrome {
      * <p>Результат: true</p>
      */
     public boolean isPalindrome(String text) {
-        return false;
+        text = text.toLowerCase();
+        text = text.replaceAll("[^a-z0-9]","");
+        return text.equals(new StringBuilder(text).reverse().toString());
     }
 }
